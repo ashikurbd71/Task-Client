7e74da253256b7c550b8cf89c04cc335ef5cd941
ashikurbd71 <ashikurovi2003@gmail.com>
2023-12-21 14:48:45 +0600

    banner added

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import image from '../assets/302l [Converted].png'
+import image from '../assets/black.jpg'
 
 const Banner = () => {
     return (
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Mainlayout from '../Layout/Mainlayout/Mainlayout';
+import Home from './../Pages/Home';
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <Mainlayout/>
-    },
+      element: <Mainlayout/>,
+     children:[{
+      path: "/",
+      element: <Home/>,
+     }],
+
+    }
   ])
 
 export default router;
